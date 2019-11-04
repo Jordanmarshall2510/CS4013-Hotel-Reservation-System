3a87cdf77734812a6297ebc4f6cf3b1355195b57
RLowry2 <47782376+RLowry2@users.noreply.github.com>
2019-11-04 18:02:57 +0000

    Update Rooms.java

--- Rooms.java
+++ Rooms.java
@@ -1,37 +1,40 @@
 public class Room{
 
-	private String type;
-	private int quantity;
-	private boolean available;
+    private String type;
+    private int quantity;
+    private boolean available;
 
-	public Room (String aType, int aQuantity, boolean isAvailable){
-		this.type = aType;
-		this.quantity = aQuantity;
-		this.available = isAvailable;
-	}
+    public Room (String aType, int aQuantity, boolean isAvailable){
+        this.type = aType;
+        this.quantity = aQuantity;
+        this.available = isAvailable;
+    }
 
-	public String getType(){
-		return type;
-	}
+    public String getType(){
+        return type;
+    }
 
-	public String setType(String aType){
-		this.type = aType;
-	}
+    public String setType(String aType){
+        this.type = aType;
+        return aType;
+    }
 
-	public int getQuantity(){
-		return quantity;
-	}
+    public int getQuantity(){
+        return quantity;
+    }
 
-	public int setQuantity(int aQuantity){
-		this.quantity = aQuantity;
-	}
+    public int setQuantity(int aQuantity){
+        this.quantity = aQuantity;
+        return aQuantity;
+    }
 
-	public boolean getAvailable(){
-		return available;
-	}
+    public boolean getAvailable(){
+        return available;
+    }
 
-	public boolean setType(boolean isAvailable){
-		this.available = isAvailable;
-	}
+    public boolean setType(boolean isAvailable){
+        this.available = isAvailable;
+        return isAvailable;
+    }
 
 }
