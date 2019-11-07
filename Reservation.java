1b0361018114160e525722b3a9c055fde88fb45b
Dermotp <47779514+Dermotp@users.noreply.github.com>
2019-11-07 12:50:40 +0000

    Create Reservation.java

---
+++ Reservation.java
@@ -0,0 +1,31 @@
+import java.time.LocalDateTime;
+
+
+/**
+ *
+ * @author Dermot Parker
+ */
+public class Reservation {
+    
+    
+   int reservationNumber;
+   int numberofNights;
+   int numberofRooms;
+   
+   String reservationName;
+   String reservationType;
+   
+   double totalCost;
+   double deposit;
+   
+   LocalDateTime checkinDate;
+   LocalDateTime checkoutDate;
+   
+   boolean breakfast;
+   
+   public Reservation()
+   {
+       
+   }
+    
+}
