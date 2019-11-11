e45befe31ff752e2feabea669a192b69b6d83a31
RLowry2 <47782376+RLowry2@users.noreply.github.com>
2019-11-11 15:22:26 +0000

    Update FiveStar.java

--- FiveStar.java
+++ FiveStar.java
@@ -1,6 +1,3 @@
-
-package Hotel_system;
-
 import java.util.ArrayList;
 
 /**
@@ -8,7 +5,7 @@ import java.util.ArrayList;
  * @author The bois
  */
 public class FiveStar extends Hotel {
-    
+
     /**
      * Number of each of these rooms that are available
      */
@@ -16,41 +13,44 @@ public class FiveStar extends Hotel {
     int numDeluxeTwin;
     int numDeluxeSingle;
     int numDeluxeFamily;
-    
+
     /**
-     * 
      * @param numberOfRooms
      * @param Rooms
      * @param numDeluxeDouble
      * @param numDeluxeTwin
      * @param numDeluxeSingle
-     * @param numDeluxeFamily 
+     * @param numDeluxeFamily
      */
     FiveStar(int numberOfRooms, ArrayList<Room> Rooms, int numDeluxeDouble,
-             int numDeluxeTwin, int numDeluxeSingle, int numDeluxeFamily )
-    {
+             int numDeluxeTwin, int numDeluxeSingle, int numDeluxeFamily) {
         this.numberOfRooms = numberOfRooms; // from Hotel
         this.Rooms = Rooms; // from Hotel
-        
+
         this.numDeluxeDouble = numDeluxeDouble;
         this.numDeluxeTwin = numDeluxeTwin;
         this.numDeluxeSingle = numDeluxeSingle;
         this.numDeluxeFamily = numDeluxeFamily;
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-        //// to do later 
+
+    FiveStar(String description)
+    {
+
+    }
+
+
+
+
+
+
+
+
+
+    //// to do later
 //    @Override
 //    public String toString()
 //    {
 //        return some strings;
 //    }
-//    
+//
 }
