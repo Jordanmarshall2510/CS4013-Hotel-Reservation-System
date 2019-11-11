313c2e060eee304b5999ebd7fd19ba994a998b65
RLowry2 <47782376+RLowry2@users.noreply.github.com>
2019-11-11 16:07:52 +0000

    Update Room.java

--- Room.java
+++ Room.java
@@ -1,65 +1,67 @@
-
-package Hotel_system;
-
 /**
  *
  * @author The bois
  */
 public class Room {
-    
+
     String Type; // room type // might change type
     double Rate; // room rate
-    
+
     int occupancyAdultMin; // Adult min for this room
     int occupancyAdultMax; // Adult max for this room
-    
+
     int occupancyChildMin; // Child min for this room
     int occupancyChildMax; // child max for this room
-    
+
     boolean breakfast; // if breakfast is included True
-    
+
     /**
-     * 
+     *
      * @param Type
      * @param Rate
      * @param occupancyAdultMin
      * @param occupancyAdultMax
      * @param occupancyChildMin
      * @param occupancyChildMax
-     * @param breakfast 
+     * @param breakfast
      */
-    Room(String Type, double Rate, int occupancyAdultMin, int occupancyAdultMax, 
-            int occupancyChildMin, int occupancyChildMax, boolean breakfast)
-         {
-             this.Type = Type;
-             this.Rate = Rate;
-             
-             this.occupancyAdultMin = occupancyAdultMin;
-             this.occupancyAdultMax = occupancyAdultMax;
-             
-             this.occupancyChildMin = occupancyChildMin;
-             this.occupancyChildMax = occupancyChildMax;
-             
-             this.breakfast = breakfast;                   
-         }
-         
-         
-         
-         
-         
-         
-         
-         
-         
-         
-         
-         
-         
-             //// to do later 
+    Room(String Type, double Rate, int occupancyAdultMin, int occupancyAdultMax,
+         int occupancyChildMin, int occupancyChildMax, boolean breakfast)
+    {
+        this.Type = Type;
+        this.Rate = Rate;
+
+        this.occupancyAdultMin = occupancyAdultMin;
+        this.occupancyAdultMax = occupancyAdultMax;
+
+        this.occupancyChildMin = occupancyChildMin;
+        this.occupancyChildMax = occupancyChildMax;
+
+        this.breakfast = breakfast;
+    }
+
+
+    Room(String description)
+    {
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    //// to do later
 //    @Override
 //    public String toString()
 //    {
 //        return some strings;
 //    }
-//    
+//
 }
