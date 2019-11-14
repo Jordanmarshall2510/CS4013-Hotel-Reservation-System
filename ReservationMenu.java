1fd131da7fbeea78e8c51b90c34b875eebaf6c70
Dermotp <47779514+Dermotp@users.noreply.github.com>
2019-11-14 18:59:59 +0000

    Major changes
    1: Menu shell has been converted to a single switch statement
    2: Making a Reservation has been simplified into a single method call makeReservation();
    3: The method calls another method that goes through a loop that promps the user to add X number of rooms pickRoom();
    4: There is an issue with using getChoice possible because the array is of type String[] rather than an Object array. Will need to make a new getChoice or edit the current one
    5: No code has been deleted only commented out

--- ReservationMenu.java
+++ ReservationMenu.java
@@ -1,4 +1,6 @@
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -6,8 +8,8 @@ import java.util.Scanner;
  * @author The bois
  */
 public class ReservationMenu {
-
-    // To be decided fully in ICT
+    
+ // To be decided fully in ICT
 
     // first Shell is " Customer(C) Employee(E) Superviser(S) "
 
@@ -24,32 +26,83 @@ public class ReservationMenu {
 
 
     private Scanner in;
+    
     private static Hotel[] reserve = { new FiveStar("Five Star"),
-            new FourStar("Four Star"),
-            new ThreeStar("Three Star")};
+                                       new FourStar("Four Star"),
+                                       new ThreeStar("Three Star")};
 
-    private static Room[] room = { new Room("Double"),
-            new Room("Twin"),
-            new Room("Single")};
+//    private static Room[] room = { new Room("Double"),
+//                                   new Room("Twin"),
+//                                   new Room("Single")};
+    
+    private static String[] rooms = new String[]{"Double",
+                                                 "Twin",
+                                                 "Single"};
+    
+    private static String[] days = new String[]{"Monday",
+                                                "Tuesday",
+                                                "Wednesday",
+                                                "Thursday",
+                                                "Friday",
+                                                "Saturday",
+                                                "Sunday"};
 
 
-    public ReservationMenu() { this.in = new Scanner(System.in); }
+    public ReservationMenu() 
+    {
+        this.in = new Scanner(System.in); 
+    }
 
-    public void Reservations() {
+    public void Reservations() 
+    {
         getChoice(reserve);
     }
-    public void roomType() {
-        getChoice(room);
+//    public void roomType() 
+//    {
+//        getChoice(rooms);
+//    }
+//    public void setNights() 
+//    {
+//        System.out.println("How Many Nights: ");
+//        int numOfNights = in.nextInt();
+//        System.out.println("How Many Adults: ");
+//        String numOfAdults = in.nextLine();
+//        System.out.println("How Many Children: ");
+//        String numOfChildren = in.nextLine();
+//        //machine.addReservation(new Reservation(numOfNights, numOfAdults, numOfChildren));
+//
+//    }
+    
+    public void pickRoom()
+    {
+        System.out.println("Pick Room type: ");
+        Object Type = getChoiceString(rooms);
+        System.out.println("Breakfast included? (1)Yes or (0)No: ");
+        int breakfast = in.nextInt();
+        //machine.addRoom(new Room(Object Type, double Rate, boolean breakfast);
+                         
     }
-    public void setNights() {
-        System.out.println("How Many Nights: ");
-        String numOfNights = in.nextLine();
-        System.out.println("How Many Adults: ");
-        String numOfAdults = in.nextLine();
-        System.out.println("How Many Children: ");
-        String numOfChildren = in.nextLine();
-        //machine.addReservation(new Reservation(numOfNights, numOfAdults, numOfChildren));
-
+    
+    public void makeReservation()
+    {
+        System.out.println("Enter your name: ");
+        String reservationName = in.nextLine();
+        System.out.println("Please enter the reservation type: (1)standard or (0)advanced: ");
+        int reservationType = in.nextInt();
+        System.out.println("Number of Nights: ");
+        int numOfNights = in.nextInt();
+        System.out.println("Number of rooms: ");
+        int numOfRooms = in.nextInt();
+        System.out.println("Choose your rooms: ");
+        for (int i = 0;i < numOfRooms; i++)
+        {
+            pickRoom();  /// problem with getchoice at the moment ///////////////////////////////////
+        }
+        System.out.println("What day would you like to check in? (DD MM YYYY): ");
+        String checkInDay = in.nextLine();
+       //machine.addReservation(new Reservation(int reservationNum, String reservationName, boolean reservationType,
+       //                       String checkInDay, int numOfNights, int numOfRooms,
+       //                       ArrayList<Room> roomsBooked, double totalCost, double Deposit)
     }
 
 
@@ -67,23 +120,36 @@ public class ReservationMenu {
             System.out.println("C)ustomer E)mployee S)upervisor Q)uit");
             String command = in.nextLine().toUpperCase();
 
-            if (command.equals("C"))
+            switch (command) 
             {
-                Reservations();
-                roomType();
-                setNights();
-            }
-            else if (command.equals("E")) //allows one coin be inserted at a time
-            {
-
-            }
-            else if (command.equals("S"))
-            {
-
-            }
-            else if (command.equals("Q"))
-            {
-                more = false;
+                case "C":
+                     Reservations();
+                     makeReservation();
+                     
+                     // roomType();
+                     // setNights();
+                    break;
+                    
+            
+                case "E":
+                    
+                    
+                    break;
+                    
+                case "S":
+                    
+                    
+                    break;
+                    
+                case "Q":
+                    
+                    
+                    more = false;
+                    break;
+                    
+                default:  
+                    
+                    break;
             }
 
         }
@@ -93,6 +159,26 @@ public class ReservationMenu {
 
 
 
+    
+    
+    
+    
+    private Object getChoiceString(String[] choices)
+    {
+        while (true)
+        {
+            char c = 'A';
+            for (String choice : choices)
+            {
+                System.out.println(c + ") " + choice);
+                c++;
+            }
+            String input = in.nextLine();
+            int n = input.toUpperCase().charAt(0) - 'A';
+            if (0 <= n && n < choices.length)
+                return choices[n];
+        }
+    }
 
     private Object getChoice(Object[] choices)
     {
@@ -115,3 +201,4 @@ public class ReservationMenu {
 
 
 }
+
