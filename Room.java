247aeb26e1ee69ea6b056cd9ebf9ef845eaba2dc
Dermotp <47779514+Dermotp@users.noreply.github.com>
2019-11-14 19:02:25 +0000

    Code has been commented out temporarily
    1: Code commented out to simplify for now
    2: Some types are changed for now
    3: Constructors reflect the changes

--- Room.java
+++ Room.java
@@ -4,16 +4,16 @@
  */
 public class Room {
 
-    String Type; // room type // might change type
+    Object Type; // room type 
     double Rate; // room rate
 
-    int occupancyAdultMin; // Adult min for this room
-    int occupancyAdultMax; // Adult max for this room
+    //int occupancyAdultMin; // Adult min for this room
+    //int occupancyAdultMax; // Adult max for this room
 
-    int occupancyChildMin; // Child min for this room
-    int occupancyChildMax; // child max for this room
+    //int occupancyChildMin; // Child min for this room
+    //int occupancyChildMax; // child max for this room
 
-    boolean breakfast; // if breakfast is included True
+    int breakfast; // if breakfast is included True
 
     /**
      *
@@ -25,21 +25,35 @@ public class Room {
      * @param occupancyChildMax
      * @param breakfast
      */
-    Room(String Type, double Rate, int occupancyAdultMin, int occupancyAdultMax,
-         int occupancyChildMin, int occupancyChildMax, boolean breakfast)
+//    Room(String Type, double Rate, int occupancyAdultMin, int occupancyAdultMax,
+//         int occupancyChildMin, int occupancyChildMax, boolean breakfast)
+//    {
+//        this.Type = Type;
+//        this.Rate = Rate;
+//
+//        this.occupancyAdultMin = occupancyAdultMin;
+//        this.occupancyAdultMax = occupancyAdultMax;
+//
+//        this.occupancyChildMin = occupancyChildMin;
+//        this.occupancyChildMax = occupancyChildMax;
+//
+//        this.breakfast = breakfast;
+//    }
+
+    
+    Room(Object Type, double Rate, int breakfast)
     {
         this.Type = Type;
         this.Rate = Rate;
 
-        this.occupancyAdultMin = occupancyAdultMin;
-        this.occupancyAdultMax = occupancyAdultMax;
+       // this.occupancyAdultMin = occupancyAdultMin;
+       // this.occupancyAdultMax = occupancyAdultMax;
 
-        this.occupancyChildMin = occupancyChildMin;
-        this.occupancyChildMax = occupancyChildMax;
+       // this.occupancyChildMin = occupancyChildMin;
+       // this.occupancyChildMax = occupancyChildMax;
 
         this.breakfast = breakfast;
     }
-
 
     Room(String description)
     {
