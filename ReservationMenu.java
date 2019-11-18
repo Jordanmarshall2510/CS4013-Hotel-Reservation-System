f15623f45829b2b1d90729b10dc462c20e5ee289
Dermotp <47779514+Dermotp@users.noreply.github.com>
2019-11-18 13:22:22 +0000

    Update ReservationMenu.java

--- ReservationMenu.java
+++ ReservationMenu.java
@@ -2,117 +2,87 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.lang.String;
 
 /**
  *
  * @author The bois
  */
 public class ReservationMenu {
-    
- // To be decided fully in ICT
-
-    // first Shell is " Customer(C) Employee(E) Superviser(S) "
 
     // Empoyee and Superviser have passwords // not to be complicated
-
-    // Customer can: Reservation(R) Cancel(C) Quit(Q)
-
-    // Empoyee can: Reservation(R) Cancel(C) CheckIn(I) CheckOut(O) Quit(Q)
-
-    // Superviser can: Reservation(R) Cancel(C) CheckIn(I) CheckOut(O) Discount(D) DataAnalysis(A) Quit(Q)
-
-    // DataAnalysis to be done with serialisation from the CSV file database // last job //
+    
+    // DataAnalysis to be done with serialisation from the CSV file database 
 
 
+    private ArrayList<Reservation> reservations = new ArrayList<>();
 
     private Scanner in;
+    
+    static int reservationNum = 1; // starts at one and increments
     
     private static Hotel[] reserve = { new FiveStar("Five Star"),
                                        new FourStar("Four Star"),
                                        new ThreeStar("Three Star")};
-
-//    private static Room[] room = { new Room("Double"),
-//                                   new Room("Twin"),
-//                                   new Room("Single")};
     
-    private static String[] rooms = new String[]{"Double",
-                                                 "Twin",
-                                                 "Single"};
+    private static String[] rooms = { new String("Double"),
+                                      new String("Twin"),
+                                      new String("Single")};
     
-    private static String[] days = new String[]{"Monday",
-                                                "Tuesday",
-                                                "Wednesday",
-                                                "Thursday",
-                                                "Friday",
-                                                "Saturday",
-                                                "Sunday"};
+    private static String[] days = { new String("Monday"),
+                                     new String("Tuesday"),
+                                     new String("Wednesday"),
+                                     new String("Thursday"),
+                                     new String("Friday"),
+                                     new String("Saturday"),
+                                     new String("Sunday")};
 
 
     public ReservationMenu() 
     {
         this.in = new Scanner(System.in); 
     }
-
-    public void Reservations() 
-    {
-        getChoice(reserve);
-    }
-//    public void roomType() 
-//    {
-//        getChoice(rooms);
-//    }
-//    public void setNights() 
-//    {
-//        System.out.println("How Many Nights: ");
-//        int numOfNights = in.nextInt();
-//        System.out.println("How Many Adults: ");
-//        String numOfAdults = in.nextLine();
-//        System.out.println("How Many Children: ");
-//        String numOfChildren = in.nextLine();
-//        //machine.addReservation(new Reservation(numOfNights, numOfAdults, numOfChildren));
-//
-//    }
     
-    public void pickRoom()
+    private void aReservation()
     {
-        System.out.println("Pick Room type: ");
-        Object Type = getChoiceString(rooms);
-        System.out.println("Breakfast included? (1)Yes or (0)No: ");
-        int breakfast = in.nextInt();
-        //machine.addRoom(new Room(Object Type, double Rate, boolean breakfast);
-                         
-    }
-    
-    public void makeReservation()
-    {
+        ArrayList<Room> roomArray = new ArrayList<>();
         System.out.println("Enter your name: ");
         String reservationName = in.nextLine();
         System.out.println("Please enter the reservation type: (1)standard or (0)advanced: ");
-        int reservationType = in.nextInt();
+        int reservationType = Integer.parseInt(in.nextLine());
+        System.out.println("What day would you like to check in? (DD MM YYYY): ");
+        String checkInDay = in.nextLine();                                       
         System.out.println("Number of Nights: ");
-        int numOfNights = in.nextInt();
+        int numOfNights = Integer.parseInt(in.nextLine());
         System.out.println("Number of rooms: ");
-        int numOfRooms = in.nextInt();
-        System.out.println("Choose your rooms: ");
+        int numOfRooms = Integer.parseInt(in.nextLine());
+        System.out.println("Choose your rooms: "+"\n");
         for (int i = 0;i < numOfRooms; i++)
         {
-            pickRoom();  /// problem with getchoice at the moment ///////////////////////////////////
+            System.out.println("Room "+ (i + 1)+ "\n");
+            System.out.println("How many adults?: ");
+            int occupancyAdultMax = Integer.parseInt(in.nextLine());
+            System.out.println("How many children?: ");
+            int occupancyChildMax = Integer.parseInt(in.nextLine());
+            System.out.println("Pick Room type: ");
+            String Type = (String) getChoice(rooms);
+            System.out.println("Breakfast included? (1)Yes or (0)No: ");
+            int breakfast = Integer.parseInt(in.nextLine());
+            roomArray.add(new Room(Type, breakfast));
         }
-        System.out.println("What day would you like to check in? (DD MM YYYY): ");
-        String checkInDay = in.nextLine();
-       //machine.addReservation(new Reservation(int reservationNum, String reservationName, boolean reservationType,
-       //                       String checkInDay, int numOfNights, int numOfRooms,
-       //                       ArrayList<Room> roomsBooked, double totalCost, double Deposit)
+        double totalCost = roomArray.size();
+        double Deposit = roomArray.size();
+        reservationNum++;
+        reservations.add(new Reservation(reservationNum, reservationName, reservationType,
+                                               checkInDay, numOfNights, numOfRooms,
+                                              roomArray, totalCost, Deposit));
     }
 
 
-
-
-
-
-    public void run(Reservation machine)
+    public void run(Reservation aReservation)
             throws IOException
-    {
+    {    
+        
         boolean more = true;
 
         while (more)
@@ -121,35 +91,155 @@ public class ReservationMenu {
             String command = in.nextLine().toUpperCase();
 
             switch (command) 
-            {
+            {               
                 case "C":
-                     Reservations();
-                     makeReservation();
-                     
-                     // roomType();
-                     // setNights();
-                    break;
-                    
-            
+                    {
+                        boolean submore = true;
+
+                        while (submore)
+                        {
+                             System.out.println("R)eserve C)ancel B)ack");
+                             String subcommand = in.nextLine().toUpperCase();
+
+                            switch (subcommand) 
+                            {    
+                                case "R":
+                                    {
+                                        aReservation();
+
+                                        break;
+                                    }
+                                case "C":
+                                    {
+                                        
+                                        break;
+                                    }
+                                case "B":
+                                    {                                       
+                                     submore = false;
+                                     break;
+                                    }
+                                 default:  
+                                    {
+                                        
+                                    break;
+                                    }
+                            }
+                        }
+                        break;
+
+                    }
                 case "E":
-                    
-                    
-                    break;
-                    
+                    {
+                        boolean submore = true;
+
+                        while (submore)
+                        {
+                             System.out.println("R)eserve C)ancel O)check Out I)check In B)ack");
+                             String subcommand = in.nextLine().toUpperCase();
+
+                            switch (subcommand) 
+                            {    
+                                case "R":
+                                    {
+                                        aReservation();
+
+                                        break;
+                                    }
+                                case "C":
+                                    {
+                                        
+                                        break;
+                                    }
+                                    case "O":
+                                    {
+                                        
+                                        break;
+                                    }
+                                    case "I":
+                                    {
+                                        
+                                        break;
+                                    }                                   
+                                case "B":
+                                    {                                      
+                                     submore = false;
+                                     break;
+                                    }
+                                 default:  
+                                    {
+                                        
+                                    break;
+                                    }
+                            }
+                        }
+                        break;
+                    }
                 case "S":
-                    
-                    
-                    break;
-                    
+                    {
+                        boolean submore = true;
+
+                        while (submore)
+                        {
+                             System.out.println("R)eserve C)ancel O)check Out I)check In D)iscount A)data Analysis B)ack");
+                             String subcommand = in.nextLine().toUpperCase();
+
+                            switch (subcommand) 
+                            {    
+                                case "R":
+                                    {
+                                        aReservation();
+
+                                        break;
+                                    }
+                                case "C":
+                                    {
+                                        
+                                        break;
+                                    }
+                                    case "O":
+                                    {
+                                        
+                                        break;
+                                    }
+                                    case "I":
+                                    {
+                                        
+                                        break;
+                                    }
+                                    case "D":
+                                    {
+                                        
+                                        break;
+                                    }
+                                    case "A":
+                                    {
+                                        
+                                        break;
+                                    }
+                                case "B":
+                                    {                                    
+                                     submore = false;
+                                     break;
+                                    }
+                                 default:  
+                                    {
+                                        
+                                    break;
+                                    }
+                            }
+                        }
+                        break;
+                    }
                 case "Q":
-                    
-                    
-                    more = false;
-                    break;
-                    
+                    {
+                        more = false;
+                        break;
+                    }
                 default:  
-                    
-                    break;
+                    {
+                        break;
+                    }
             }
 
         }
@@ -157,28 +247,32 @@ public class ReservationMenu {
     }
 
 
-
-
-    
-    
-    
-    
-    private Object getChoiceString(String[] choices)
+    public void addReservation(Reservation aReservation)
     {
-        while (true)
-        {
-            char c = 'A';
-            for (String choice : choices)
-            {
-                System.out.println(c + ") " + choice);
-                c++;
-            }
-            String input = in.nextLine();
-            int n = input.toUpperCase().charAt(0) - 'A';
-            if (0 <= n && n < choices.length)
-                return choices[n];
-        }
+        reservations.add(aReservation);
     }
+
+
+    
+    
+    
+    /// currently unused ////
+//    private Object getChoiceString(String[] choices)
+//    {
+//        while (true)
+//        {
+//            char c = 'A';
+//            for (String choice : choices)
+//            {
+//                System.out.println(c + ") " + choice);
+//                c++;
+//            }
+//            String input = in.nextLine();
+//            int n = input.toUpperCase().charAt(0) - 'A';
+//            if (0 <= n && n < choices.length)
+//                return choices[n];
+//        }
+//    }
 
     private Object getChoice(Object[] choices)
     {
@@ -201,4 +295,3 @@ public class ReservationMenu {
 
 
 }
-
