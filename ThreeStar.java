a51735cfa532fbf671c5ea747fbabfa6fdbfb21d
Dermotp <47779514+Dermotp@users.noreply.github.com>
2019-11-19 15:19:53 +0000

    Update ThreeStar.java

--- ThreeStar.java
+++ ThreeStar.java
@@ -1,61 +1,5 @@
 import java.util.ArrayList;
 
-/**
- *
- * @author The bois
- */
-public class ThreeStar extends Hotel {
-
-    /**
-     * Number of each of these rooms that are available
-     */
-    int numClassicDouble;
-    int numClassicTwin;
-    int numClassicSingle;
-
-    /**
-     *
-     * @param numberOfRooms
-     * @param Rooms
-     * @param numClassicDouble
-     * @param numClassicTwin
-     * @param numClassicSingle
-     */
-    ThreeStar(int numberOfRooms, ArrayList<Room> Rooms, int numClassicDouble,
-              int numClassicTwin, int numClassicSingle )
-    {
-        this.numberOfRooms = numberOfRooms; // from Hotel
-        this.Rooms = Rooms; // from Hotel
-
-        this.numClassicDouble = numClassicDouble;
-        this.numClassicTwin = numClassicTwin;
-        this.numClassicSingle = numClassicSingle;
-    }
-
-
-    ThreeStar(String description)
-    {
-
-    }
-
-
-
-
-
-
-
-
-
-
-    //// to do later
-//    @Override
-//    public String toString()
-//    {
-//        return some strings;
-//    }
-//
-}import java.util.ArrayList;
-
 
 /**
  *
@@ -96,4 +40,5 @@ public class ThreeStar extends Hotel {
     }
 
 }
+
 
