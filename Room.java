210c4429cd5d90938e8aaf8afcfec3f76d29be0b
Dermotp <47779514+Dermotp@users.noreply.github.com>
2019-11-19 15:14:03 +0000

    Update Room.java

--- Room.java
+++ Room.java
@@ -1,8 +1,19 @@
+
 /**
  *
  * @author The bois
  */
 public class Room {
+
+    @Override
+    public String toString() {
+        return  "Room = " + Type +
+                ", Rate = " + Rate +
+                ", Adults = " + occupancyAdultMax +
+                ", Children = " + occupancyChildMax +
+                ", breakfast = " + breakfast +
+                ", booked = " + booked + '}';
+    }
 
     String Type; // room type 
     double Rate; // room rate
@@ -16,41 +27,14 @@ public class Room {
     int breakfast; // if breakfast is included True
     boolean booked; // true if this room is currently booked
 
-    /**
-     *
-     * @param Type
-     * @param Rate
-     * @param occupancyAdultMin
-     * @param occupancyAdultMax
-     * @param occupancyChildMin
-     * @param occupancyChildMax
-     * @param breakfast
-     */
-//    Room(String Type, double Rate, int occupancyAdultMin, int occupancyAdultMax,
-//         int occupancyChildMin, int occupancyChildMax, boolean breakfast)
-//    {
-//        this.Type = Type;
-//        this.Rate = Rate;
-//
-//        this.occupancyAdultMin = occupancyAdultMin;
-//        this.occupancyAdultMax = occupancyAdultMax;
-//
-//        this.occupancyChildMin = occupancyChildMin;
-//        this.occupancyChildMax = occupancyChildMax;
-//
-//        this.breakfast = breakfast;
-//    }
-
     
     Room(String Type, double Rate, int breakfast)
     {
         this.Type = Type;
         this.Rate = Rate;
 
-       // this.occupancyAdultMin = occupancyAdultMin;
         this.occupancyAdultMax = occupancyAdultMax;
 
-       // this.occupancyChildMin = occupancyChildMin;
         this.occupancyChildMax = occupancyChildMax;
 
         this.breakfast = breakfast;
@@ -64,32 +48,7 @@ public class Room {
     Room(String Type, int breakfast)
     {
         this.Type = Type;
-        //this.Rate = Rate;
-
-       // this.occupancyAdultMin = occupancyAdultMin;
-       // this.occupancyAdultMax = occupancyAdultMax;
-
-       // this.occupancyChildMin = occupancyChildMin;
-       // this.occupancyChildMax = occupancyChildMax;
-
         this.breakfast = breakfast;
     }
 
-
-
-
-
-
-
-
-
-
-
-    //// to do later
-//    @Override
-//    public String toString()
-//    {
-//        return some strings;
-//    }
-//
 }
