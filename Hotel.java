d0d953d764b3ae37228e91a439033ade02cc59ff
RLowry2 <47782376+RLowry2@users.noreply.github.com>
2019-11-21 18:26:10 +0000

    Update Hotel.java

--- Hotel.java
+++ Hotel.java
@@ -1,18 +1,13 @@
-
 import java.util.ArrayList;
 
-/**
- *
- * @author The bois 
- * 
- * This class is abstract and will never be invoked in its own right
- */
 public abstract class Hotel {
-    
+
     protected int numberOfRooms; // Each hotel type uses its own
-    
+
     protected ArrayList<Room> Rooms = new ArrayList<>(); // Each hotel type uses its own
-    
-    
- 
+
+    public static Hotel[] reserve = { new FiveStar("Five Star"),
+            new FourStar("Four Star"),
+            new ThreeStar("Three Star")};
+
 }
